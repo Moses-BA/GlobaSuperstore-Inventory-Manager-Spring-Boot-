a38071c03984f5c0c0669e327b0efabcd58fc8b4
Moses BA <138978717+Moses-BA@users.noreply.github.com>
2023-12-15 11:38:04 +0000

    Add files via upload

---
+++ Inventory Web/src/main/java/com/ltp/globalsuperstore/Item.java
@@ -0,0 +1,66 @@
+package com.ltp.globalsuperstore;
+
+import java.sql.Date;
+import java.util.UUID;
+
+public class Item {
+    private String category;
+    private String name;
+    private Double price;
+    private Double discount;
+    private Date date;
+    private String id;
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getDiscount() {
+        return this.discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Item() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+}
